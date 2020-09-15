31a3f0245b7d095d54fd34ce057539370f5c6345
Miłosz <mpasekprivate@gmail.com>
2020-09-15 12:38:35 +0200

    added a new component that is now displaying Poke info

--- src/HomeSection/HomeSection.js
+++ src/HomeSection/HomeSection.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 import RandomButton from './RandomButton/RandomButton'
+import ScreenArea from './ScreenArea/ScreenArea'
 import classes from './HomeSection.module.css'
 
 
@@ -9,15 +10,17 @@ class HomeSection extends Component {
   state = {
     url: 'https://pokeapi.co/api/v2/pokemon/',
     pokeName: '',
-    sprite: "Loading poke sprite"
+    sprite: "Loading poke sprite",
+    pokeId: 50
   }
 
   componentDidMount() {
-    axios.get(this.state.url + 50)
+    axios.get(this.state.url + this.state.pokeId)
       .then(res => {        
         this.setState({
           pokeName: res.data.name,
-          sprite: res.data.sprites.front_default
+          sprite: res.data.sprites.front_default,
+          pokeId: res.data.id
         })
         console.log(res.data.sprites.front_default)
       })
@@ -36,7 +39,8 @@ class HomeSection extends Component {
       .then(res => {
         this.setState({
           pokeName: res.data.name,
-          sprite: res.data.sprites.front_default
+          sprite: res.data.sprites.front_default,
+          pokeId: res.data.id
         })
         console.log(res.data.sprites.front_default)
       })
@@ -46,9 +50,16 @@ class HomeSection extends Component {
     return (
       <div className={classes.Container}>
         <p>content of home section here</p>
-        <p>Name: {this.state.pokeName.charAt(0).toUpperCase() + this.state.pokeName.slice(1)}</p>
-        <img src={this.state.sprite} alt={this.state.pokeName} />
-        <RandomButton randomButtonClicked={this.randomButtonClickHandler} />
+        <ScreenArea 
+          name={this.state.pokeName}
+          id={this.state.pokeId}
+          sprite={this.state.sprite}
+        />
+        <div className={classes.ButtonsContainer}>
+          <RandomButton randomButtonClicked={this.randomButtonClickHandler} />
+          <RandomButton randomButtonClicked={this.randomButtonClickHandler} />
+          <RandomButton randomButtonClicked={this.randomButtonClickHandler} />
+        </div>
       </div>
     )
   }
