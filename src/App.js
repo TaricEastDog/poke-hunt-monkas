d649bc723e44a2f020ab10a48494e87eb12554f0
TaricEastDog <malzusiek666@gmail.com>
2020-11-19 21:11:16 +0100

    dummy commit

--- src/App.js
+++ src/App.js
@@ -1,94 +1,95 @@
-import React, {Component} from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-import Navbar from './Navbar/Navbar'
-import HomeSection from './HomeSection/HomeSection'
-import Collection from './Collection/Collection'
-import About from './About/About'
-import FAQ from './FAQ/FAQ'
-import classes from './App.module.css';
+import Navbar from "./Navbar/Navbar";
+import HomeSection from "./HomeSection/HomeSection";
+import Collection from "./Collection/Collection";
+import About from "./About/About";
+import FAQ from "./FAQ/FAQ";
+import classes from "./App.module.css";
+
+// some dummy data to commit
 
 class App extends Component {
   state = {
-    url: 'https://pokeapi.co/api/v2/pokemon/',
-    pokeName: '',
+    url: "https://pokeapi.co/api/v2/pokemon/",
+    pokeName: "",
     sprite: "Loading poke sprite",
     pokeId: 50,
     pokeList: [],
-    currentPage: 'home',
-    bgImage: 'https://www.wallpaperflare.com/static/905/635/699/pok%C3%A9mon-video-games-retro-games-pokemon-wallpaper.jpg'
-  }
+    currentPage: "home",
+    bgImage:
+      "https://www.wallpaperflare.com/static/905/635/699/pok%C3%A9mon-video-games-retro-games-pokemon-wallpaper.jpg",
+  };
 
   navBarClickHandler = (nav) => {
-    this.setState({currentPage: nav})
-  }
+    this.setState({ currentPage: nav });
+  };
 
   componentDidMount() {
-    axios.get(this.state.url + this.state.pokeId)
-      .then(res => {        
-        this.setState({
-          pokeName: res.data.name,
-          sprite: res.data.sprites.front_default,
-          pokeId: res.data.id
-        })
-        console.log(res.data.sprites.front_default)
-      })
+    axios.get(this.state.url + this.state.pokeId).then((res) => {
+      this.setState({
+        pokeName: res.data.name,
+        sprite: res.data.sprites.front_default,
+        pokeId: res.data.id,
+      });
+      console.log(res.data.sprites.front_default);
+    });
   }
 
   randomButtonClickHandler = () => {
-    let rand = Math.random() * 151 + 1  
-    rand = Math.floor(rand)
-    axios.get(this.state.url + rand)
-      .then(res => {
-        this.setState({
-          pokeName: res.data.name,
-          sprite: res.data.sprites.front_default,
-          pokeId: res.data.id
-        })
-        console.log(res.data.sprites.front_default)
-      })
-  }
+    let rand = Math.random() * 151 + 1;
+    rand = Math.floor(rand);
+    axios.get(this.state.url + rand).then((res) => {
+      this.setState({
+        pokeName: res.data.name,
+        sprite: res.data.sprites.front_default,
+        pokeId: res.data.id,
+      });
+      console.log(res.data.sprites.front_default);
+    });
+  };
 
   catchButtonHandler = () => {
-    this.setState({pokeList: [...this.state.pokeList, this.state.pokeId]})
-  }
+    this.setState({ pokeList: [...this.state.pokeList, this.state.pokeId] });
+  };
 
   feedButtonClickHander = () => {
-    console.log('you fed a Poke')
-  }
+    console.log("you fed a Poke");
+  };
 
-  render () {
-    const homeSection = <HomeSection 
-    catchButtonClickHandler={this.catchButtonHandler} 
-    randomButtonClickHandler={this.randomButtonClickHandler}
-    feedButtonClickHander={this.feedButtonClickHander}
-    pokeId={this.state.pokeId}
-    pokeName={this.state.pokeName}
-    pokeList={this.state.pokeList}
-    sprite={this.state.sprite}
-    />
-    const collection = <Collection pokeList={this.state.pokeList} />
+  render() {
+    const homeSection = (
+      <HomeSection
+        catchButtonClickHandler={this.catchButtonHandler}
+        randomButtonClickHandler={this.randomButtonClickHandler}
+        feedButtonClickHander={this.feedButtonClickHander}
+        pokeId={this.state.pokeId}
+        pokeName={this.state.pokeName}
+        pokeList={this.state.pokeList}
+        sprite={this.state.sprite}
+      />
+    );
+    const collection = <Collection pokeList={this.state.pokeList} />;
 
-    let currentDisplay = homeSection
+    let currentDisplay = homeSection;
 
-    if (this.state.currentPage === 'home') {
-      currentDisplay = homeSection
-    } else if (this.state.currentPage === 'collection') {
-      currentDisplay = collection
-    } else if (this.state.currentPage === 'about') {
-      currentDisplay = <About bgImage={this.state.bgImage} />
+    if (this.state.currentPage === "home") {
+      currentDisplay = homeSection;
+    } else if (this.state.currentPage === "collection") {
+      currentDisplay = collection;
+    } else if (this.state.currentPage === "about") {
+      currentDisplay = <About bgImage={this.state.bgImage} />;
     } else {
-      currentDisplay = <FAQ />
+      currentDisplay = <FAQ />;
     }
 
     return (
       <div className={classes.App}>
         <Navbar navBarClickHandler={this.navBarClickHandler} />
-          <div className={classes.Container}>
-          {currentDisplay}
-          </div>
+        <div className={classes.Container}>{currentDisplay}</div>
       </div>
-    )
+    );
   }
 }
 
